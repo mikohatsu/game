39ed89b32fc6aa5d7a63a10dbbaa33c6e6abb841
mikohatsu <mikohatsu@users.noreply.github.com>
2025-12-01 19:21:54 +0900

    Include all game files in deployment

--- text_dungeon_react/vite.config.js
+++ text_dungeon_react/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/text_dungeon/',
+  base: '/game/text_dungeon/',
   build: {
     outDir: 'dist'
   }
