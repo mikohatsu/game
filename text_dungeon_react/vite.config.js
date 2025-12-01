d909d480ae81f06a2280b6f1273d1114e027ded1
mikohatsu <mikohatsu@users.noreply.github.com>
2025-12-01 19:13:22 +0900

    fix: depoly url

--- text_dungeon_react/vite.config.js
+++ text_dungeon_react/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/game/',
+  base: '/text_dungeon/',
   build: {
-    outDir: '../dist/text-dungeon'
+    outDir: 'dist'
   }
 })
